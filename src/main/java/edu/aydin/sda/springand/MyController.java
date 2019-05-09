e14d6971215053651e44c9f81bc3543a6e125b4d
henesgenc <36183669+henesgenc@users.noreply.github.com>
2019-05-09 11:54:40 +0300

    Not used imports deleted

--- src/main/java/edu/aydin/sda/springand/MyController.java
+++ src/main/java/edu/aydin/sda/springand/MyController.java
@@ -1,16 +1,11 @@
 package edu.aydin.sda.springand;
 
-import org.springframework.http.HttpRequest;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import edu.aydin.sda.Entity.Products;
 import edu.aydin.sda.Entity.Stocks;
@@ -19,13 +14,9 @@ import edu.aydin.sda.Util.Utill;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
